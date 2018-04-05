67d83bda6ecc4be764a946a9b279a2fb72168269
TMVF <malithf95@gmail.com>
2018-04-05 12:45:16 +0530

    CRUD

---
+++ src/app/employees/employee-list/employee-list.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+
+import { EmployeeService } from '../shared/employee.service';
+import { Employee } from '../shared/employee.model';
+import { element } from 'protractor';
+import { ToastrService } from 'ngx-toastr';
+
+
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
+})
+export class EmployeeListComponent implements OnInit {
+   employeelist : Employee[];
+  constructor(private employeeService : EmployeeService,private tostr : ToastrService) { }
+
+  ngOnInit() {
+    var x = this.employeeService.getData();
+    x.snapshotChanges().subscribe(item => {
+       this.employeelist = [];
+       item.forEach(element => {
+         var y = element.payload.toJSON();
+         y["$key"] = element.key;
+         this.employeelist.push(y as Employee);
+       });
+    });
+  }
+
+  onEdit(emp : Employee){
+    this.employeeService.selectedEmployee = Object.assign({},emp);
+     
+  }
+
+  onDelete(key : string){
+    if (confirm('Are you sure to delete this record ?') == true){
+    this.employeeService.deleteEmployee(key);
+    this.tostr.warning("Deleted Successfuly", "Employee Resgister");
+  }
+
+}
+
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+      apiKey: "AIzaSyBGBiK-BUz72PavUVkvGgDQHOHtz2TTc00",
+      authDomain: "angular5curd-2d73e.firebaseapp.com",
+      databaseURL: "https://angular5curd-2d73e.firebaseio.com",
+      projectId: "angular5curd-2d73e",
+      storageBucket: "angular5curd-2d73e.appspot.com",
+      messagingSenderId: "383503092382"
+    }
+  };
+
